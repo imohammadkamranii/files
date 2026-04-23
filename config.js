fafe200c37a278bf242bc91e02c59e08c938db64
mohamad kamrani <111233059+imohammadkamranii@users.noreply.github.com>
2026-04-23 21:19:59 +0330

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-export const CONFIG = {
-  TG_TOKEN: process.env.TG_TOKEN,
-  BALE_TOKEN: process.env.BALE_TOKEN,
-  WEBHOOK_PATH: "/api/webhook",
-};
-
-export const USER_MAPPING = {
-     "7623362575": "831819531"
-};
