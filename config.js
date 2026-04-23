18e9c34d696722dedae54b51b68b35d55c44aa58
mohamad kamrani <111233059+imohammadkamranii@users.noreply.github.com>
2026-04-23 21:06:52 +0330

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const CONFIG = {
+  TG_TOKEN: process.env.TG_TOKEN,
+  BALE_TOKEN: process.env.BALE_TOKEN,
+  WEBHOOK_PATH: "/api/webhook",
+};
+
+export const USER_MAPPING = {
+     "7623362575": "831819531"
+};
